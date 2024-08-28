9a7c3d741d36ac41a853a6f1295856afebb2817f
SurendharSSenthil <surendharsenthil257@gmail.com>
2024-08-28 20:49:39 +0530

    iv sem commit

--- my-express/config/db.js
+++ my-express/config/db.js
@@ -1,13 +1,13 @@
-const mongoose = require('mongoose');
-const { MONGODB_CONNECTION_STRING } = require('./index');
+const mongoose = require("mongoose");
+const { MONGODB_CONNECTION_STRING } = require("./index");
 
 mongoose.connect(MONGODB_CONNECTION_STRING, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-  dbName: 'StudentDB'
+	useNewUrlParser: true,
+	useUnifiedTopology: true,
+	dbName: "course_evaluation-IV-sem",
 });
 
 const db = mongoose.connection;
-db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+db.on("error", console.error.bind(console, "MongoDB connection error:"));
 
 module.exports = db;
--- my-express/index.js
+++ my-express/index.js
@@ -1,8 +1,8 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const cors = require('cors');
-require('dotenv').config();
-const mongoose = require('mongoose');
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+require("dotenv").config();
+const mongoose = require("mongoose");
 
 const app = express();
 const port = 4000;
@@ -11,22 +11,24 @@ app.use(bodyParser.json());
 app.use(cors());
 
 if (!process.env.MONGODB_CONNECTION_STRING) {
-  console.error('MONGODB_CONNECTION_STRING is not set. Please set it in your environment.');
-  process.exit(1);
+	console.error(
+		"MONGODB_CONNECTION_STRING is not set. Please set it in your environment."
+	);
+	process.exit(1);
 }
 
 mongoose.connect(process.env.MONGODB_CONNECTION_STRING, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-  dbName: 'StudentDB'
+	useNewUrlParser: true,
+	useUnifiedTopology: true,
+	dbName: "course_evaluation-IV-sem",
 });
 
-const studentRoutes = require('./routes/studentRoutes');
-const adminRoutes = require('./routes/adminRoutes');
+const studentRoutes = require("./routes/studentRoutes");
+const adminRoutes = require("./routes/adminRoutes");
 
-app.use('/api/student', studentRoutes);
-app.use('/api/admin', adminRoutes);
+app.use("/api/student", studentRoutes);
+app.use("/api/admin", adminRoutes);
 
 app.listen(port, () => {
-  console.log(`Express Listening on ${port}`);
+	console.log(`Express Listening on ${port}`);
 });
